8b3e85c6581738778d7401b5d91fffd00b3b4ec9
HaroldMagos <engrhm02@gmail.com>
2025-02-26 13:03:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("access-button").addEventListener("click", function() {
     // Replace this URL with your Google Script deployed app URL
-    const googleScriptURL = "https://script.google.com/macros/s/AKfycbwmD1_atB-klBxMAexZoblR_qCKDr6RjpX5YfLC1k73uayleV1M4V4jajPHybO99SFaBA/exec";
+    const googleScriptURL = "https://script.google.com/macros/s/AKfycbw6n6kR5yKDJCso2TWFqsfuyVq3shazHw8onTWmbcQl0BTQfZg-vXglBIIDZQ8KsSTBeQ/exec";
     window.location.href = googleScriptURL;
 });
